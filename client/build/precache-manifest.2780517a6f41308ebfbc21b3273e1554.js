d86129e44ce704ba17e55f0daa12715420e7a60d
Aleksandr130402 <businessmail1304@gmail.com>
2020-09-11 22:28:03 +0300

    Added some changes

---
+++ client/build/precache-manifest.2780517a6f41308ebfbc21b3273e1554.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad588b729e72311ae188",
+    "url": "/static/css/main.ab3abf92.chunk.css"
+  },
+  {
+    "revision": "ad588b729e72311ae188",
+    "url": "/static/js/main.4e9d3cde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "163d3894ac98297926d1",
+    "url": "/static/css/2.a5091408.chunk.css"
+  },
+  {
+    "revision": "163d3894ac98297926d1",
+    "url": "/static/js/2.890e9e09.chunk.js"
+  },
+  {
+    "revision": "897ea9f0c8feea8bc513a0c0ac89ada1",
+    "url": "/static/media/photo_3.897ea9f0.jpg"
+  },
+  {
+    "revision": "22824024a2a80dd69a9646a7f8db1327",
+    "url": "/static/media/portfolio-1.22824024.png"
+  },
+  {
+    "revision": "723f349ab2734ad0518b7fd11758c06f",
+    "url": "/static/media/portfolio-2.723f349a.png"
+  },
+  {
+    "revision": "40ea65f4126faa53f1247d9bb61faa41",
+    "url": "/static/media/portfolio-3.40ea65f4.png"
+  },
+  {
+    "revision": "b166aafd24433dddd9f90e2c24f84375",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.204706da7ca69ec1198022b40eff8210.js"
+  "/precache-manifest.2780517a6f41308ebfbc21b3273e1554.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
